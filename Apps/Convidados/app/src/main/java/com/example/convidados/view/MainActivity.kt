62cc48413b08f1dd37795425bb220d779b4c7736
devmasterteam <devmasterteam@gmail.com>
2020-03-23 01:48:04 -0300

    Adição app Convidados. Adição doc de padrões e slides de aulas

---
+++ Apps/Convidados/app/src/main/java/com/example/convidados/view/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.convidados.view
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.example.convidados.R
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAppBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener {
+            startActivity(Intent(applicationContext, GuestFormActivity::class.java))
+        }
+
+        setupNavigation()
+    }
+
+    // Responsável por criar o menu superior direito (3 pontinhos)
+    /* override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    } */
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(mAppBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigation() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        mAppBarConfiguration =
+            AppBarConfiguration.Builder(
+                    R.id.nav_all,
+                    R.id.nav_presents,
+                    R.id.nav_absents
+                )
+                .setDrawerLayout(drawerLayout)
+                .build()
+        setupActionBarWithNavController(navController, mAppBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
