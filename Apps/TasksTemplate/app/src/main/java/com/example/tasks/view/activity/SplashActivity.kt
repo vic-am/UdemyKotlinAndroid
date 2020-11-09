92e32dfa146ec675c4f8d8694fc4226f1c8f94c5
Victor Amaral <vic_amaral_23@hotmail.com>
2020-11-09 00:21:22 -0300

    Adiciona recursos que serão usados no serviço da API.

---
+++ Apps/TasksTemplate/app/src/main/java/com/example/tasks/view/activity/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tasks.view.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tasks.R
+import java.util.*
+
+
+class SplashActivity : AppCompatActivity() {
+
+    private val timer = Timer()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                jump()
+            }
+        }, 3000)
+
+    }
+
+    private fun jump() {
+        timer.cancel()
+        startActivity(
+            Intent(
+                this@SplashActivity,
+                RegisterActivity::class.java
+            )
+        )
+        finish()
+    }
+
+}
--- Apps/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
+++ Apps/TasksTemplate/app/src/main/java/com/example/tasks/viewmodel/LoginViewModel.kt
@@ -2,18 +2,41 @@ package com.example.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.tasks.service.constants.TaskConstants.SHARED.PERSON_KEY
+import com.example.tasks.service.constants.TaskConstants.SHARED.PERSON_NAME
+import com.example.tasks.service.constants.TaskConstants.SHARED.TOKEN_KEY
+import com.example.tasks.service.listener.ApiListener
+import com.example.tasks.service.model.HeaderModel
+import com.example.tasks.service.repository.PersonRepository
+import com.example.tasks.service.repository.local.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    /**
-     * Faz login usando API
-     */
+    private val personRepository = PersonRepository()
+    private val sharedPreferences = SecurityPreferences(application)
+
+    private val mutableLiveLogin = MutableLiveData<Boolean>()
+    var login: LiveData<Boolean> = mutableLiveLogin
+
     fun doLogin(email: String, password: String) {
+        personRepository.login(email, password, object : ApiListener {
+            override fun onSuccess(model: HeaderModel) {
+                sharedPreferences.store(TOKEN_KEY, model.token)
+                sharedPreferences.store(PERSON_KEY, model.personKey)
+                sharedPreferences.store(PERSON_NAME, model.name)
+
+                mutableLiveLogin.value = true
+            }
+
+            override fun onFailure(message: String) {
+                mutableLiveLogin.value = false
+            }
+
+        })
     }
 
-    /**
-     * Verifica se usuário está logado
-     */
     fun verifyLoggedUser() {
     }
 
